0dade8b758e9029835f4925540867d7f4d48b5b2
unknown <mohitv@EXZINDLAP027.exzeo.net>
2015-03-13 00:15:54 +0530

    made account controller

---
+++ CodeCompiler/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CodeCompiler.Controllers
+{
+     public class AccountController : Controller
+     {
+
+          /// <summary>
+          /// Login page
+          /// </summary>
+          /// <returns></returns>
+          public ActionResult Index()
+          {
+               return View();
+          }
+
+
+          /// <summary>
+          /// View for user signUp
+          /// </summary>
+          /// <returns></returns>
+          public ActionResult SignUp()
+          {
+               return View();
+          }
+
+
+          /// <summary>
+          /// Process signup of users with valid credentails
+          /// </summary>
+          /// <returns></returns>
+          [HttpPost]
+          [ValidateAntiForgeryToken]
+          public JsonResult CompleteSignUp()
+          {
+               return Json( new { Data = string.Empty, status = 200 } );
+          }
+
+
+          /// <summary>
+          /// Process login of users with valid credentails
+          /// </summary>
+          /// <returns></returns>
+          [HttpPost]
+          [ValidateAntiForgeryToken]
+          public JsonResult Login()
+          {
+               return Json( new { Data = string.Empty, status = 200 } );
+          }
+
+     }
+}
--- CodeCompiler/Controllers/HomeController.cs
+++ CodeCompiler/Controllers/HomeController.cs
@@ -10,63 +10,63 @@ namespace CodeCompiler.Controllers
      public class HomeController : Controller
      {
 
-          public ActionResult Index()
+          [ValidateInput( false )]
+          public JsonResult CompileCode(string code, string language)
           {
-               return View(new Output());
-          }
+               try
+               {
+                    var fileDetail = FileHandler.ConvertTextToCodeFile( null, code, language );
+                    var output = new Compiler( language ).Compile( fileDetail, language );
 
-
-          [ValidateInput(false)]
-          public string CompileCode(string code, string language)
-          {
-               try {
-                    var fileDetail = FileHandler.ConvertTextToCodeFile(null, code, language);
-                    var output = new Compiler(language).Compile(fileDetail, language);
-
-                    if (output.status) {
+                    if (output.status)
+                    {
                          output.message = "Compiled Successfully";
-                         return JsonConvert.SerializeObject(output);
+                         return Json( new { Data = string.Empty, status = 200 } );
                     }
                     else
-                         return JsonConvert.SerializeObject(output);
+                         return Json( new { Data = string.Empty, status = 200 } );
 
                }
 
-               catch {
-                    return JsonConvert.SerializeObject(new Output() { status = false, message = "Compiler is busy" });
+               catch
+               {
+                    return Json( new { Data = string.Empty, status = 500 } );
                }
 
           }
 
 
-          [ValidateInput(false)]
-          public string RunCode(string code, string language)
+          [ValidateInput( false )]
+          public JsonResult RunCode(string code, string language)
           {
-               try {
-                    var fileDetail = FileHandler.ConvertTextToCodeFile(null, code, language);
-                    ICompiler compiler = new Compiler(language);
-                    var output = compiler.Compile(fileDetail, language);
+               try
+               {
+                    var fileDetail = FileHandler.ConvertTextToCodeFile( null, code, language );
+                    ICompiler compiler = new Compiler( language );
+                    var output = compiler.Compile( fileDetail, language );
 
-                    if (output.status) {
+                    if (output.status)
+                    {
                          string inputData = FileHandler.GetInput();
                          string outputData = FileHandler.GetActualOutput();
 
-                         output = compiler.ProgramOutput(fileDetail, inputData, language);
-                         output.message = FileHandler.CleanText(output.message);
-                         outputData = FileHandler.CleanText(outputData);
+                         output = compiler.ProgramOutput( fileDetail, inputData, language );
+                         output.message = FileHandler.CleanText( output.message );
+                         outputData = FileHandler.CleanText( outputData );
 
                          output.status = output.message == outputData;
 
-                         return JsonConvert.SerializeObject(output);
+                         return Json( new { Data = string.Empty, status = 500 } );
                     }
 
                     else
-                         return JsonConvert.SerializeObject(output);
+                         return Json( new { Data = string.Empty, status = 500 } );
 
                }
 
-               catch {
-                    return JsonConvert.SerializeObject(new Output() { status = false, message = "Compiler is busy" });
+               catch
+               {
+                    return Json( new { Data = string.Empty, status = 500 } );
                }
           }
 
